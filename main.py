1ec302a9dd69040524e096717458ab60db7cf715
Gabriel <gbrldssnts@gmail.com>
2024-02-10 21:04:03 -0300

    New version 1.0.1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
-  print("1.0.1 version")
-  print("here should change")
+ 
+ print("1.0.1 version")
+  print("create a change on file")
+  print("create another print")
+
 
 if __main__ == "__name__":
     main()
