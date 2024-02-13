d16eeaac485f8cba4768bb551c9352925d381fa2
Gabriel <gbrldssnts@gmail.com>
2024-02-13 19:35:54 -0300

    test about dict in python

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def main():
- 
- print("1.0.1 version")
- print("now, i want this print")
+# This is just a test about dictionary in python
 
+def main():
+  
+  def ola():
+    return "ola tudo bem"
+
+  def xau():
+    return "Adeus"
+
+  comandos = dict(ola=ola(),xau=xau())
+
+  sub = input("digite ola ou xau")
+
+  print(comandos.get(sub))
+    
 
 if __name__ == "__main__":
     main()
